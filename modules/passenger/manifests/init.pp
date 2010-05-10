ce9dc189ef653b45aff53f9a2d9e06ac51826ab7
Tom Ward <tom@popdog.net>
2010-05-10 20:44:15 +0100

    Updated passenger module to strip control codes from build output so it can be used directly by apache.

--- modules/passenger/manifests/init.pp
+++ modules/passenger/manifests/init.pp
@@ -1,20 +1,24 @@
 class passenger {
-    include apache
+  include apache
 
-    service { "passenger":
-        require => Package["passenger"]
-    }
+  service { "passenger":
+    require => Package["passenger"]
+  }
 
-    package { "passenger":
-        ensure => latest,
-        provider => "gem",
-        require => Service["apache2"]
-    }
+  package { "passenger":
+    ensure => latest,
+    provider => "gem",
+    require => Service["apache2"]
+  }
+      
+  # This command is a bit extreme, so here's a brief idea of what it does.  passenger-install-apache-module
+  # compiles the apache module and outputs how to integrate it.  This is piped into a sed command to remove
+  # all whitespace, and then the three apache directives are extracted with grep
     
-    exec { "/usr/local/bin/passenger-install-apache2-module --auto | grep -e 'LoadModule' -e 'PassengerRoot' -e 'PassengerRuby' > /etc/apache2/mods-available/passenger.load":
-        subscribe => Package["passenger"],
-        refreshonly => true
-    }
+  exec { "/usr/local/bin/passenger-install-apache2-module --auto | sed -e 's/[[:cntrl:]]\[[[:digit:]]m//' | grep -e 'LoadModule' -e 'PassengerRoot' -e 'PassengerRuby' > /etc/apache2/mods-available/passenger.load":
+    subscribe => Package["passenger"],
+    refreshonly => true
+  }
     
   define host($host) {
     file { "$host":
