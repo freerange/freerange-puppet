94115e378d6ab23c8e1680b6b43a1c4b3743648d
Tom Ward <tom@popdog.net>
2010-05-19 08:11:56 +0100

    Use brightbox REE packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,25 @@
-# Install ruby enterprise edition from the package
-wget http://rubyforge.org/frs/download.php/68720/ruby-enterprise_1.8.7-2010.01_amd64.deb
-dpkg -i ruby-enterprise*.deb
-rm ruby-enterprise*.deb
+# Add brightbox as a source for apt (and get the brightbox key).  Brightbox provide a packaged version
+# of ruby enterprise edition, replacing the standard ubuntu ruby
+wget http://apt.brightbox.net/release.asc -O - | apt-key add -
+echo "deb http://apt.brightbox.net/ lucid rubyee" > /etc/apt/sources.list.d/brightbox-rubyee.list
 
-# Update rubygems to the latest version
-gem update --system
-
-# Install puppet
-gem install --no-rdoc --no-ri -y puppet
-mkdir -p /var/lib/puppet
- 
-# Install git
+# Update apt sources, and install ruby, rdoc, rubygems and irb
 apt-get update
+apt-get install -y ruby rdoc rubygems irb libopenssl-ruby  
+apt-get install -y libruby1.8 irb1.8 libopenssl-ruby1.8 libreadline-ruby1.8 rdoc1.8 ruby1.8
+
+# Install git
 apt-get install -y git-core
 
+# Update rubygems to the latest version.  On ubuntu this is harder than it might normally
+# be as gem update --system fails.
+gem install rubygems-update
+/var/lib/gems/1.8/bin/update_rubygems
+
+# Install puppet
+gem install --no-rdoc --no-ri puppet -v 0.25.5
+mkdir -p /var/lib/puppet
+ 
 # Get latest freerange-puppet stuff
 git clone http://github.com/freerange/freerange-puppet.git /etc/puppet
  
--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -20,6 +20,7 @@ node default {
   
   ssh_authorized_key {"tom@popdog.net":
     ensure => present,
+    user => freerange,
     key => "AAAAB3NzaC1yc2EAAAABIwAAAQEA7wy/hGcJZ1PFRCjs0UKIrALs+zm0d2+fVtHJqCdWTOOOaP3NRUEsi5Eb60vzuTkgEilcOhIT9jMUMCUzKHbqlG6EMhXFOsyaNDV30oyj9pHYeqUDCY52vb9GXTejFEFAy4bnlG+5N//B8nXiGuOjTWJdXuDhhvFSO1Cqp9doDQLfgnfP8pUmquVHYVN7aOIjIMlmnhXedLOAWhmfNaaA6IrbsqEqRAqe+YqRtB25Kqh4UCd8Fjd7dh98W32TrLtlh8qtb8e7U3W+lskpnbDubcptQKAdzbKDFpdpPQKGMtrCnPx3rnddZWH2dV36smm+IwwUbrjH3U/1F2ci8aCgFQ==",
     name => "tomw@popdog.net",
     target => "/home/freerange/.ssh/authorized_keys",
@@ -29,6 +30,7 @@ node default {
 
   ssh_authorized_key {"deploy@gofreerange.com":
     ensure => present,
+    user => freerange,
     key => "AAAAB3NzaC1yc2EAAAABIwAAAQEAuTgZ+wS8YJUmB6c+F8QimQh/xZuWPYa3fXmLDw2kp8S2cAf6XqU5Fuod/wrhlBdzGTjJdRm3CMWdCUDrh/D68Op8uaIqf/h78eLcVkC7DL3SfFqFxZ/IpQsUXEiFII5rjz2y3orLvUABuIJkuY72DoWhcn6Y0j8uy/wg94QXWKfLdPZTE4kBokO2QmLB+WYsoPAWdaUpswplfidhN1WwQWVHSV7Nfh2Tnym4rMXG61O7mYrFTRKEx5o3eSb9USBj2bEGVo+Nj8YWgNQK4+jxvmQ27kuMhF2C/4rQEXNqcPJnQa9gDpEQttfP8ZW0LNemvfNM/J6Mw05fcPvF5iAigw==",
     name => "deploy@gofreerange.com",
     target => "/home/freerange/.ssh/authorized_keys",
