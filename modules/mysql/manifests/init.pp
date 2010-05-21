42ae2471bf0eb1cce7bdbf5a359b939f559c5cbb
Tom Ward <tom@popdog.net>
2010-05-21 08:01:51 +0100

    Added some simple monitoring to mysql module

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,7 +1,51 @@
 class mysql {
   class server {
+    include monit
+    
+    $mysql_user = "root"
+    $mysql_password = generate("/usr/bin/makepasswd")
+
+    user { "mysql":
+      ensure => present,
+      require => Package["mysql-server"],
+    }
+    
     package {"mysql-server":
       ensure => latest
+    }
+
+    service { "mysql":
+      ensure => running,
+      require => Package["mysql-server"]
+    }
+    
+    file { "/etc/mysql/my.cnf":
+      require => Package["mysql-server"],
+      source => "/etc/puppet/modules/mysql/files/my.cnf",
+      notify => Service["mysql"],
+      owner => root,
+      group => root
+    }
+
+    exec { "Initialize MySQL server root password":
+      unless      => "/usr/bin/test -f /root/.my.cnf",
+      command     => "/usr/bin/mysqladmin -u${mysql_user} password ${mysql_password}",
+      notify      => Exec["Generate /root/.my.cnf"],
+      require     => [Package["mysql-server"], Service["mysql"]]
+    }
+
+    exec { "Generate /root/.my.cnf":
+      command     => "/bin/echo -e \"[mysql]\nuser=${mysql_user}\npassword=${mysql_password}\n[mysqladmin]\nuser=${mysql_user}\npassword=${mysql_password}\n[mysqldump]\nuser=${mysql_user}\npassword=${mysql_password}\n[mysqlshow]\nuser=${mysql_user}\npassword=${mysql_password}\n\" > /root/.my.cnf",
+      refreshonly => true,
+      creates     => "/root/.my.cnf",
+    }
+    
+    file { "/etc/monit.d/mysql.monit": 
+      ensure => present,
+      source => "/etc/puppet/modules/mysql/files/mysql.monit",
+      owner => root,
+      group => root,
+      notify => Service["monit"]
     }
   }
 }
