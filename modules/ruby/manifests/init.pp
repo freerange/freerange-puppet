7809e5ede39f4322cfb87ca15bcdccb08dd1e80d
Tom Ward <tom@popdog.net>
2010-06-18 10:09:33 +0100

    Don't use REE until it includes fix for http://redmine.ruby-lang.org/issues/show/2739

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -1,32 +1,35 @@
-# wget http://apt.brightbox.net/release.asc -O - | apt-key add -
-# echo "deb http://apt.brightbox.net/ lucid rubyee" > 
-# 
-# 
 class ruby {
   include rubygems
-  
-  file {"brightbox":
-    path => "/etc/apt/sources.list.d/brightbox-rubyee.list",
-    ensure => present,
-    owner => root,
-    group => root,
-    content => "deb http://apt.brightbox.net/ lucid rubyee",
-    notify => Exec["update apt"]
-  }
-  
-  exec {"brightbox":
-    command => "/usr/bin/wget http://apt.brightbox.net/release.asc -O - | /usr/bin/apt-key add -",
-    unless => "/usr/bin/apt-key list | /bin/grep Brightbox",
-    notify => Exec["update apt"]
-  }
-  
+
+  # At the moment, the version of 1.8.7 REE (2010.02) is based on has a nasty bug that
+  # causes puppet to fail when running (see http://redmine.ruby-lang.org/issues/show/2739).
+  # This bug has been fixed in MRI 1.8.7 but not yet released so for now the brightbox REE
+  # stuff is disabled, until this bug fix makes its way to Brightbox's REE.
+
+  # When this happens, sections below can be uncommented
+
+  # file {"brightbox":
+  #   path => "/etc/apt/sources.list.d/brightbox-rubyee.list",
+  #   ensure => present,
+  #   owner => root,
+  #   group => root,
+  #   content => "deb http://apt.brightbox.net/ lucid rubyee",
+  #   notify => Exec["update apt"]
+  # }
+
+  # exec {"brightbox":
+  #   command => "/usr/bin/wget http://apt.brightbox.net/release.asc -O - | /usr/bin/apt-key add -",
+  #   unless => "/usr/bin/apt-key list | /bin/grep Brightbox",
+  #   notify => Exec["update apt"]
+  # }
+
   exec {"update apt":
     command => "/usr/bin/apt-get update",
-    refreshonly => true  
-  }  
-  
+    refreshonly => true
+  }
+
   package {["rdoc", "ruby", "irb", "rubygems", "libopenssl-ruby", "irb1.8", "libruby1.8", "libopenssl-ruby1.8", "libreadline-ruby1.8", "ruby1.8", "rdoc1.8", "ruby1.8-dev"]:
     ensure => latest,
-    require => [File["brightbox"], Exec["brightbox"]]
+  # require => [File["brightbox"], Exec["brightbox"]]
   }
 }
