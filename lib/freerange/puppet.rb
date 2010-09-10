3aeb50c6afe241bff1fe75009f8047152ebc3507
Tom Ward <tom@popdog.net>
2010-09-10 11:39:39 +0100

    Updated capistrano deployment recipes to push puppet classes to server, rather than pulling from git

--- lib/freerange/puppet.rb
+++ lib/freerange/puppet.rb
@@ -2,26 +2,20 @@ Capistrano::Configuration.instance(:must_exist).load do
   default_run_options[:pty]     = true # needed for git password prompts
   ssh_options[:forward_agent]   = true # use the keys for the person running the cap command to check out the app
 
-  set :user, "freerange"
   set :puppet_dryrun, false
   set :puppet_debug, false
+  set :puppet_path, '/tmp/puppet_recipes'
   
   namespace :puppet do
-    desc 'Bootstrap server, installing puppet and git'
-    task :bootstrap do
-      run "wget -q -O - http://github.com/freerange/freerange-puppet/raw/master/bootstrap.sh | sh"
-      puppet.apply
-    end
-    
-    desc 'Update puppet configuration from repository'
-    task :update do
-      #run "cd /etc/puppet; #{sudo} git pull"
+    desc "Deploy our puppet recipes to the server"
+    task :deploy_recipes do
+      run "rm -rf #{puppet_path}"
+      upload File.expand_path("../../../puppet", __FILE__), puppet_path
     end
     
     desc 'Apply puppet configuration'
     task :apply do
-      puppet.update
-      apply_manifest "/etc/puppet/manifests/site.pp"
+      apply_manifest "#{puppet_path}/site.pp"
     end
     
     desc 'Dryrun puppet configuration'
@@ -35,12 +29,10 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
   end
   
-  before "deploy:setup", "puppet:apply"
-
   def apply_manifest(manifest, options = {})
     dryrun_option = fetch('puppet_dryrun') ? "--noop " : ""
     debug_option = fetch('puppet_debug') ? "-d " : ""
-    run "#{sudo} puppet #{dryrun_option}-v #{debug_option}#{manifest}", options
+    run "puppet --templatedir #{puppet_path}/classes #{dryrun_option}-v #{debug_option}#{manifest}", options
   end
   
   def manifest(role, manifest)
@@ -49,9 +41,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     name = "#{role}-manifest"
     top.namespace :puppet do
       task name, :roles => [role.to_sym] do
-        sudo "mkdir -p /etc/puppet/manifests/apps"
-        put ERB.new(manifest).result(binding), "/tmp/#{application}.#{role}.pp", :roles => [role.to_sym]
-        sudo "mv /tmp/#{application}.#{role}.pp /etc/puppet/manifests/apps/#{application}.#{role}.pp", :roles => [role.to_sym]
+        put ERB.new(manifest).result(binding), "#{puppet_path}/roles/#{application}-#{role}.pp", :roles => [role.to_sym]
       end
       
       before "puppet:apply" do
