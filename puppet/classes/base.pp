f50784e313c004a6888d1b27124bfc9f70282c21
Tom Ward <tom@popdog.net>
2010-09-13 07:54:22 +0100

    Set server timezones to Europe/London

--- puppet/classes/base.pp
+++ puppet/classes/base.pp
@@ -1,7 +1,18 @@
-class base {  
-  file { "/etc/hosts":
-    content => template("base/hosts"),
-    owner => root,
-    group => root
+class base {
+  include base::hosts
+  include base::time
+
+  class hosts {
+    file { "/etc/hosts":
+      content => template("base/hosts"),
+      owner => root,
+      group => root
+    }
+  }
+
+  class time {
+    file { "/etc/localtime":
+      source => "/usr/share/zoneinfo/Europe/London"
+    }
   }
 }
