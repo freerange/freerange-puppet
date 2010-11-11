9d7da337d98c3d24e51ad0a84523730a3b8cfa4d
James Adam <james@lazyatom.com>
2010-11-11 14:31:37 +0000

    We can use the redis-server package for ubuntu.
    On lucid this is 1.2.0, although we were installing a 1.3 version.
    
    1.2 doesn't seem to be able to 'include' other config files, which is a pain because each run of puppet will append our configuration to the end of redis.conf.
    
    We might look at upgrading to redis 2.0, since that is now stable (and presumably allows including). There doesn't seem to be a redis-server 2.0 package for lucid though.

--- puppet/classes/redis.pp
+++ puppet/classes/redis.pp
@@ -1,53 +1,26 @@
 class redis {
-  $version = 'v1.3.12'
-  $version_path = "/usr/local/src/redis-$version"
-  $bin = "/usr/local/bin"
-  
-  file {"/usr/local/src/redis-$version":
-    ensure => "directory",
-    owner => root,
-    group => root
-  }
-  
-  exec {"extract-redis": 
-    command =>"wget http://github.com/antirez/redis/tarball/$version -O redis_$version.tar.gz && tar --strip-components 1 -xzvf redis_$version.tar.gz",
-    cwd => "$version_path",
-    creates => "$version_path/redis.c",
-    require => File["$version_path"],
-    before => Exec["make-redis"]
-  }
-  
-  exec { "make-redis":
-    command => "sh -c \"cd $version_path && make && mv redis-server $bin/ && mv redis-cli $bin/ && mv redis-benchmark $bin/ && mv redis-check-dump $bin/\"",
-    creates => "$bin/redis-server",
+  package {"redis-server":
+    ensure => present
   }
 
-  file { "/etc/init.d/redis":
-    content => template("redis/redis-init-script"),
-    require => Exec["make-redis"],
-    mode => 700
+  exec { "bind-local":
+     command => "echo 'bind 127.0.0.1' >> /etc/redis/redis.conf",
+     require => Package["redis-server"]
   }
 
-  exec { "enable-redis-service":
-    command => "sh -c \"chkconfig --add redis\"",
-    require => File["/etc/init.d/redis"]
+  exec { "appendonly-yes":
+    command => "echo 'appendonly yes' >> /etc/redis/redis.conf",
+    require => Package["redis-server"]
   }
 
-  file { "/etc/redis":
-    ensure => directory,
-    require => Exec["make-redis"]
+  exec { "appendfsync-everysec":
+    command => "echo 'appendfsync everysec' >> /etc/redis/redis.conf",
+    require => Package["redis-server"]
   }
 
-  file { "/etc/redis/redis.conf":
-    ensure => present,
-    content => template("redis/redis.conf"),
-    require => File["/etc/redis"],
-    notify => Service["redis"]
-  }
-
-  service { "redis":
+  service { "redis-server":
     ensure => running,
-    require => File["/etc/init.d/redis"],
+    require => [Package["redis-server"], Exec["bind-local"], Exec["appendonly-yes"], Exec["appendfsync-everysec"]],
     enable => true
   }
 }
