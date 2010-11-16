0b70fc8ac77cf39c0592ce4464d0b5ff1587080f
Chris Roos & Kalvir Sandhu <pairing+chris.roos+kalvir.sandhu@gofreerange.com>
2010-11-16 13:12:35 +0000

    We couldn't create a user with multiple SSH keys which was preventing us deploying as a single user.

--- puppet/classes/freerange.pp
+++ puppet/classes/freerange.pp
@@ -2,6 +2,22 @@ class freerange {
   include zsh
 
   define user($user, $key, $key_type="ssh-rsa") {
+    user_without_ssh_key { $name:
+      user => $user
+    }
+    append_ssh_key_to_user { $name:
+      user => $user,
+      key => $key,
+      key_type => $key_type
+    }
+    append_ssh_key_to_user { "freerange-$name":
+      user => "freerange",
+      key => $key,
+      key_type => $key_type
+    }
+  }
+
+  define user_without_ssh_key($user) {
     include base::application
 
     user {$user:
@@ -15,15 +31,15 @@ class freerange {
       group => application,
       require => User[$user]
     }
+  }
 
+  define append_ssh_key_to_user($user, $key, $key_type="ssh-rsa") {
     ssh_authorized_key { $name:
       ensure => present,
       user => $user,
       key => $key,
       name => $name,
-      target => "/home/$user/.ssh/authorized_keys",
-      type => $key_type,
-      require => [File["/home/$user"], User[$user]],
+      type => $key_type
     }
   }
 }
