00bee9dc126983dfcf631616e3dcf9555abb396d
James Mead <james@floehopper.org>
2011-01-21 15:07:12 +0000

    By parameterizing the redis class and using an ERB template, we can be more declarative, avoid appending configuration overrides to redis.conf every puppet:apply, and avoid unnecessary restarts of the redis service.

--- puppet/classes/redis.pp
+++ puppet/classes/redis.pp
@@ -1,21 +1,20 @@
-class redis {
+class redis($bind_address = "127.0.0.1", $appendonly = "no", $appendfsync = "always") {
+
   package {"redis-server":
     ensure => "2:1.2.0-1"
   }
 
-  exec { "appendonly-yes":
-    command => "echo 'appendonly yes' >> /etc/redis/redis.conf",
-    require => Package["redis-server"]
-  }
-
-  exec { "appendfsync-everysec":
-    command => "echo 'appendfsync everysec' >> /etc/redis/redis.conf",
-    require => Package["redis-server"]
+  file { "/etc/redis/redis.conf":
+    content => template("redis/redis.conf.erb"),
+    owner => root,
+    group => root,
+    require => Package["redis-server"],
+    notify => Service["redis-server"]
   }
 
   service { "redis-server":
     ensure => running,
-    require => [Package["redis-server"], Exec["appendonly-yes"], Exec["appendfsync-everysec"]],
+    require => Package["redis-server"],
     enable => true
   }
 }
